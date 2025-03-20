605dfae41f562c452bd3ad962b515d552788c460
alex <famaldonadoos@gmail.com>
2025-03-20 08:38:56 -0500

    changed robot scale factor

--- Plugins/ExperimentPlugin/Source/ExperimentPlugin/Client/ExperimentClient.h
+++ Plugins/ExperimentPlugin/Source/ExperimentPlugin/Client/ExperimentClient.h
@@ -319,7 +319,7 @@ public:
 	/* ==== world stuff ==== */
 	int FrameCount        = 0; // todo: will probably delete 
 	const float MapLength = 235.185;
-	const float PredatorScaleFactor = 0.25f; 
+	const float PredatorScaleFactor = 0.1f; 
 	float WorldScale      = 15.0f;
 
 	UPROPERTY(EditAnywhere)
